ddab3034c9aaecb01c4849a672ecfef23fe76799
Wellidontcare <djjorisdj@gmail.com>
2020-01-30 20:49:23 +0100

    refactored timer

--- backend/timer/timer.cpp
+++ backend/timer/timer.cpp
@@ -3,13 +3,10 @@
 Timer::Timer(QObject *parent) :
     QObject(parent),
     start_time(std::chrono::high_resolution_clock::now())
-{
-}
+{}
 
-Timer::~Timer()
-{
-    emit meassured_time(QString::number
-                        (std::chrono::duration_cast<std::chrono::microseconds>
-                         (std::chrono::high_resolution_clock::now() - start_time).count())
-                        + "mu");
+Timer::~Timer(){
+    auto time_taken = std::chrono::duration_cast<std::chrono::microseconds>
+            (std::chrono::high_resolution_clock::now() - start_time).count();
+    emit meassured_time(QString::number(time_taken)+ "mu");
 }
